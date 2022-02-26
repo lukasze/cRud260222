f9a6e6df7c285e5e832ed7c34c983f4e8a8929f8
uki <wyspianski.lukasz@gmail.com>
2022-02-26 12:31:27 +0100

    Inicjalizacja projektu: Web, Data JPA, H2, Lombok

---
+++ src/main/java/com/example/crud260222/CRud260222Application.java
@@ -0,0 +1,13 @@
+package com.example.crud260222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CRud260222Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CRud260222Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crud260222/CRud260222ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud260222;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CRud260222ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
